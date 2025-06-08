33ff30640bf5be62b7bb15a3ebb01314bb7da9bd
Amanda <phototix@hotmail.com>
2025-06-09 01:59:35 +0800

    Fix image filtering.

--- listImages.php
+++ listImages.php
@@ -1,22 +1,34 @@
 <?php
 $imageFolder = 'E:/MAMP/htdocs/stream/snapshots/';
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$filter = isset($_GET['filter']) ? $_GET['filter'] : 'all';
 $perPage = 18;
 $offset = ($page - 1) * $perPage;
 
-$imageFiles = glob($imageFolder . '*.jpg');
-$totalImages = count($imageFiles);
+// Get all matching files based on filter
+$allFiles = glob($imageFolder . '*.jpg');
+$filteredFiles = [];
+
+foreach ($allFiles as $file) {
+    $filename = basename($file);
+    if ($filter === 'all' || strpos($filename, $filter . '_') === 0) {
+        $filteredFiles[] = $file;
+    }
+}
+
+$totalImages = count($filteredFiles);
 $totalPages = ceil($totalImages / $perPage);
 
-$paginatedFiles = array_slice($imageFiles, $offset, $perPage);
+// Paginate the filtered results
+$paginatedFiles = array_slice($filteredFiles, $offset, $perPage);
 $images = [];
 
 foreach ($paginatedFiles as $file) {
     $filename = basename($file);
     $nameWithoutExt = pathinfo($filename, PATHINFO_FILENAME);
     $images[] = [
-        'thumb' => '/stream/snapshots/' . $filename,
-        'full' => '/stream/snapshots/' . $filename,
+        'thumb' => 'snapshots/' . $filename,
+        'full' => 'snapshots/' . $filename,
         'name' => $nameWithoutExt
     ];
 }
