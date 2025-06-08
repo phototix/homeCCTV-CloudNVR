d877f6ef19aac3fbe2e75555928daa0dd23d7344
Amanda <phototix@hotmail.com>
2025-06-09 01:51:42 +0800

    Add pagination

--- listImages.php
+++ listImages.php
@@ -1,9 +1,17 @@
 <?php
 $imageFolder = 'E:/MAMP/htdocs/stream/snapshots/';
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$perPage = 18;
+$offset = ($page - 1) * $perPage;
+
 $imageFiles = glob($imageFolder . '*.jpg');
+$totalImages = count($imageFiles);
+$totalPages = ceil($totalImages / $perPage);
+
+$paginatedFiles = array_slice($imageFiles, $offset, $perPage);
 $images = [];
 
-foreach ($imageFiles as $file) {
+foreach ($paginatedFiles as $file) {
     $filename = basename($file);
     $nameWithoutExt = pathinfo($filename, PATHINFO_FILENAME);
     $images[] = [
@@ -14,5 +22,14 @@ foreach ($imageFiles as $file) {
 }
 
 header('Content-Type: application/json');
-echo json_encode($images);
+echo json_encode([
+    'images' => $images,
+    'pagination' => [
+        'page' => $page,
+        'perPage' => $perPage,
+        'totalImages' => $totalImages,
+        'totalPages' => $totalPages,
+        'hasMore' => $page < $totalPages
+    ]
+]);
 ?>
